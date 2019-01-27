f7f5d6d68a4ed0526bac18057a4c9e1f7d78ad11
Jaapio <jaap@ijaap.nl>
2019-01-27 11:44:18 +0100

    Allow installing extra packages when needed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,15 @@
 #!/bin/sh -l
 
+=======
+if [[ ! -z "$PHP_EXTENSIONS" ]]; then
+  echo "installing $PHP_EXTENSIONS"
+  apk add -q --no-cache $PHP_EXTENSIONS;
+  rm -rf /var/cache/apk/*;
+fi
+
+if [[ ! -z "$COMPOSER_PACKAGES" ]]; then
+   echo "installing $COMPOSER_PACKAGES"
+   composer global require --quiet $COMPOSER_PACKAGES;
+fi
+
 sh -c "composer install --no-scripts --no-progress && /composer/vendor/bin/phpstan $*"
