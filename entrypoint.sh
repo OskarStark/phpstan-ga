ce1f1c1cf546a0a356df294bcfac41c30d66cd31
core23 <mail@core23.de>
2019-12-21 11:51:18 +0100

    Silence composer task

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-sh -c "composer install --no-scripts --no-progress && /composer/vendor/bin/phpstan $*"
+sh -c "composer install --no-scripts --no-progress --quiet && /composer/vendor/bin/phpstan $*"
