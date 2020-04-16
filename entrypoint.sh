62701c8c75ae7a0d3689dd202df7aad611db28ce
Oskar Stark <oskarstark@googlemail.com>
2020-04-16 08:55:52 +0200

    Enhancement: Support variable for --ignore-platform-reqs

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,17 @@ echo "::group::Installed PHPStan extensions"
 composer show | grep phpstan
 echo "::endgroup::"
 
+IGNORE_PLATFORM_REQS=""
+if [ "$CHECK_PLATFORM_REQUIREMENTS" = "false" ]; then
+    IGNORE_PLATFORM_REQS="--ignore-platform-reqs"
+fi
+
 NO_DEV="--no-dev"
 if [ "$REQUIRE_DEV" = "true" ]; then
     NO_DEV=""
 fi
-COMPOSER_COMMAND="composer install --no-scripts --no-progress $NO_DEV"
+
+COMPOSER_COMMAND="composer install --no-scripts --no-progress $NO_DEV $IGNORE_PLATFORM_REQS"
 echo "::group::$COMPOSER_COMMAND"
 $COMPOSER_COMMAND
 echo "::endgroup::"
